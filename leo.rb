4173e3bf4d69bb777ec4b4a641e1583eda2001d2
Michał Pociecha-Łoś <micapolos@gmail.com>
2021-06-24 16:21:16 +0200

    Update SHA256 and depends_on

--- leo.rb
+++ leo.rb
@@ -2,6 +2,9 @@ class Leo < Formula
   desc "Leo programming language"
   homepage "https://github.com/micapolos/leo-script"
   url "https://github.com/micapolos/leo-script/archive/refs/tags/0.25.zip"
+  sha256 "3389c762e36469f4fe8667d1a666dfcb40451b97c773c0a7ceca235f6227d9aa"
+
+  depends_on "openjdk"
 
   def install
     bin.install "/bin/*"
