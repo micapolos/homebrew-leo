0ffcdc95c0f600ba97c3d581d76918052727e0cd
Michał Pociecha-Łoś <micapolos@gmail.com>
2021-06-24 16:23:59 +0200

    Update depends_on java

--- leo.rb
+++ leo.rb
@@ -4,10 +4,10 @@ class Leo < Formula
   url "https://github.com/micapolos/leo-script/archive/refs/tags/0.25.zip"
   sha256 "3389c762e36469f4fe8667d1a666dfcb40451b97c773c0a7ceca235f6227d9aa"
 
-  depends_on "openjdk"
+  depends_on "java"
 
   def install
-    bin.install "/bin/*"
-    lib.install "/lib/*"
+    bin.install "bin/*"
+    lib.install "lib/*"
   end
 end
