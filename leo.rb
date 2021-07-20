d34b5c1015eb5e999ffa014daea81467d40f28e8
Michał Pociecha-Łoś <micapolos@gmail.com>
2021-07-20 17:15:07 +0200

    0.25.20

--- leo.rb
+++ leo.rb
@@ -1,8 +1,8 @@
 class Leo < Formula
   desc "Leo programming language"
   homepage "https://github.com/micapolos/leo-script"
-  url "https://github.com/micapolos/leo-script/releases/download/v0.25.19/leo.zip"
-  sha256 "a6f9b70ed367c98915b7b3bedcf53d595e9c8a8cd75d754f9936b9113fbb6289"
+  url "https://github.com/micapolos/leo-script/releases/download/v0.25.20/leo.zip"
+  sha256 "2b05d888c91a9ccd3eac74549f51198fce0bf1fc97ebfb781d64be4675b6dd72"
 
   depends_on "openjdk"
 
