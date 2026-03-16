c3d5a7fc30286b794155a0a64cca5980c6dc50d5
Michał Pociecha-Łoś <micapolos@gmail.com>
2026-03-16 17:37:55 +0100

    leo v0.1.3

--- leo.rb
+++ leo.rb
@@ -1,13 +1,10 @@
 class Leo < Formula
   desc "Leo programming language"
-  homepage "https://github.com/micapolos/leo-script"
-  url "https://github.com/micapolos/leo-script/files/6707971/leo-0.25.zip"
-  sha256 "2c0571ef5575a72ae8b37c289b395721997e48d94861c1cd924f82420ac8ee2d"
-
-  depends_on "java"
+  homepage "https://github.com/micapolos/micascheme"
+  url "https://github.com/micapolos/micascheme/releases/download/v0.1.3/leo-tarm64osx-v0.1.3.tar.gz"
+  sha256 "24099eb24511adada0b5c69b5c9e3a49b19d7f078e9c9b619dfc554c38ad63f1"
 
   def install
     bin.install "bin/leo" => "leo"
-    prefix.install Dir["lib"]
   end
 end
