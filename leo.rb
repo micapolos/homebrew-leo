22c031541cbdc79b00563594a48ff1b2d50e8d25
Michał Pociecha-Łoś <micapolos@gmail.com>
2026-03-16 19:17:23 +0100

    install link

--- leo.rb
+++ leo.rb
@@ -7,6 +7,7 @@ class Leo < Formula
   def install
     libexec.install "bin", "lib", "examples"
     (bin/"leo").write_exec_script (libexec/"bin/leo")
+    bin.install_symlink_bin bin/"leo" if (bin/"leo").exist?
   end
 
   test do
