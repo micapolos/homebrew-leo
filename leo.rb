10489f502107b5b2421f7457f02d5bdf89134bd1
Michał Pociecha-Łoś <micapolos@gmail.com>
2026-03-16 19:01:53 +0100

    write exec script

--- leo.rb
+++ leo.rb
@@ -2,18 +2,14 @@ class Leo < Formula
   desc "Programming language"
   homepage "https://github.com/micapolos/micascheme"
   url "https://github.com/micapolos/micascheme/releases/download/v0.1.4/leo-tarm64osx-v0.1.4.tar.gz"
-  sha256 "sha256:da8c75becea134f71e121e2c3d927fe94dd6f4110d7713356ac7f4a1feb441de"
+  sha256 "da8c75becea134f71e121e2c3d927fe94dd6f4110d7713356ac7f4a1feb441de"
 
   def install
     libexec.install "bin", "lib", "examples"
-    bin.install_symlink libexec/"bin/leo"
+    (bin/"leo").write_exec_script (libexec/"bin/leo")
   end
 
   test do
-    assert_path_exists prefix/"bin"
-    assert_path_exists prefix/"lib"
-    assert_path_exists prefix/"examples"
-
     system "#{bin}/leo"
   end
 end
