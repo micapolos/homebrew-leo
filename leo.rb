178c5619eed5c40320e66b16a1b25222e6b04163
Michał Pociecha-Łoś <micapolos@gmail.com>
2026-03-17 02:48:58 +0100

    man

--- leo.rb
+++ leo.rb
@@ -5,7 +5,9 @@ class Leo < Formula
   sha256 "7b925a0c4d06ed4f8ff4f11d8ace6d029c9b3d3fa6c03ed3aaa2925e9cfbc34a"
 
   def install
-    libexec.install "bin", "lib", "examples"
+    libexec.install "bin", "lib"
+    pkgshare.install "examples"
+    man.install Dir["man/*"]
     bin.write_exec_script (libexec/"bin/leo")
   end
 
