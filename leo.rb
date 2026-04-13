78ed742dc34f6870acbfaa5cdaf18c75ebd2df92
Michał Pociecha-Łoś <micapolos@gmail.com>
2026-04-13 02:41:53 +0200

    v0.1.93

--- leo.rb
+++ leo.rb
@@ -1,8 +1,8 @@
 class Leo < Formula
   desc "Programming language"
   homepage "https://github.com/micapolos/micascheme/tree/main/leo"
-  url "https://github.com/micapolos/micascheme/releases/download/v0.1.92/leo-tarm64osx-v0.1.92.tar.gz"
-  sha256 "55c9cea0317e2577423e6e1471f275ead2e466722da252fa01a462dbd10039e1"
+  url "https://github.com/micapolos/micascheme/releases/download/v0.1.93/leo-tarm64osx-v0.1.93.tar.gz"
+  sha256 "82feb09eca28aa79027be728997da813f1c6ce9a821b738b5111ce6a8808d70e"
 
   def install
     libexec.install "bin", "lib"
